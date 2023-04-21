b9ebdf158ca77636a41389c5325dd30fa949bdad
David Hernandez <desarrollo5@albertoalvarez.com>
2023-04-20 20:08:33 -0500

    Agrego contenido y reactividad al grip de historial

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,40 @@
 <template>
-    <div>Hola mundo!</div>
+    <Layout>
+        <template #header>
+            <Header/>
+        </template>
+        <template #resume>
+            <Resume
+                :label="label"
+                :labelTotal="'Ahorro total'"
+                :totalAmount="1000000"
+                :amount="amount"
+            />
+        </template>
+        <template #movements>
+            <Movements/>
+        </template>
+    </Layout>
 </template>
+<script>
+import Layout from "./Layout.vue";
+import Header from "./Header.vue";
+import Resume from "./Resume/Index.vue";
+import Movements from "./Movements.vue";
+
+export default{
+    components:{
+        Header,
+        Layout,
+        Resume,
+        Movements
+    },
+    data(){
+        return {
+            amount: null,
+            label: null
+        }
+    }
+}
+
+</script>
---
+++ src/components/Movements.vue
@@ -0,0 +1,3 @@
+<template>
+    <div class="movements">Historial</div>
+</template>
